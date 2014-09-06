259f0005ff412b43a7ebf7fd938e1039ff460125
sparhami <sepandparhami@gmail.com>
2014-09-06 14:50:53 -0700

    Added unit tests

---
+++ test/unit/dependencies/testDepsTrackingLoader.js
@@ -0,0 +1,148 @@
+/*jshint expr: true*/
+
+describe('DepsTrackingLoader', function() {
+    var sandbox = sinon.sandbox.create();
+
+    beforeEach(function() {
+        sandbox.stub(Loader.prototype, 'get');
+        sandbox.stub(Loader.prototype, 'load');
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+    describe('loadDeps', function() {
+        it('should call load for all the dependencies', function() {
+            var depsStorage = {
+                    getDeps: sinon.stub().withArgs('src').returns(['one', 'two'])
+                },
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            depsTrackingLoader.load = sinon.stub();
+            depsTrackingLoader.loadDeps('src');
+
+            expect(depsTrackingLoader.load)
+                .to.have.been.calledWith('one')
+                .to.have.been.calledWith('two');
+        });
+    });
+
+    describe('updateDeps', function() {
+        it('should persist the dependencies to storage', function() {
+            var depsStorage = {
+                    setDeps: sinon.spy()
+                },
+                doc = {
+                    querySelectorAll: sinon.stub().returns([
+                        {
+                            href: 'one'
+                        },
+                        {
+                            href: 'two'
+                        }
+                    ])
+                },
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            depsTrackingLoader.load = sinon.stub();
+            depsTrackingLoader.updateDeps('src', doc);
+
+            expect(depsStorage.setDeps).to.have.been.calledWith('src', ['one', 'two']);
+        });
+
+        it('should call load for each of the dependencies', function() {
+            var depsStorage = {
+                    setDeps: sinon.spy()
+                },
+                doc = {
+                    querySelectorAll: sinon.stub().returns([
+                        {
+                            href: 'one'
+                        },
+                        {
+                            href: 'two'
+                        }
+                    ])
+                },
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            depsTrackingLoader.load = sinon.stub();
+            depsTrackingLoader.updateDeps('src', doc);
+
+            expect(depsTrackingLoader.load)
+                .to.have.been.calledWith('one')
+                .to.have.been.calledWith('two');
+        });
+    });
+
+    describe('load', function() {
+        it('should not attempt to load a source that is already loaded', function() {
+            var depsTrackingLoader = new DepsTrackingLoader();
+
+            Loader.prototype.get.returns(true);
+
+            depsTrackingLoader.load('src');
+
+            expect(Loader.prototype.load).to.not.have.been.called;
+        });
+
+        it('should call the provided callback after loading', function() {
+            var depsStorage = {
+                    getDeps: sinon.stub().returns([])
+                },
+                callback = sinon.spy(),
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            Loader.prototype.get.returns(false);
+            Loader.prototype.load.yields({
+                target: {}
+            });
+
+            depsTrackingLoader.updateDeps = sinon.stub();
+            depsTrackingLoader.load('src', callback);
+
+            expect(callback).to.have.been.calledOnce;
+        });
+
+        it('should call updateDeps after loading', function() {
+            var depsStorage = {
+                    getDeps: sinon.stub().returns([])
+                },
+                doc = {},
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            Loader.prototype.get.returns(false);
+            Loader.prototype.load.yields({
+                target: {
+                    'import': doc
+                }
+            });
+
+            depsTrackingLoader.updateDeps = sinon.stub();
+            depsTrackingLoader.load('src');
+
+            expect(depsTrackingLoader.updateDeps).to.have.been.calledWith('src', doc);
+        });
+
+        it('should load all the deps for a given src', function() {
+            var depsStorage = {
+                    getDeps: sinon.stub()
+                },
+                depsTrackingLoader = new DepsTrackingLoader(depsStorage);
+
+            Loader.prototype.get.returns(false);
+
+            depsStorage.getDeps.withArgs('src').returns(['one', 'two']);
+            depsStorage.getDeps.withArgs('one').returns([]);
+            depsStorage.getDeps.withArgs('two').returns([]);
+
+            depsTrackingLoader.load('src');
+
+            expect(Loader.prototype.load)
+                .to.have.been.calledWith('src')
+                .to.have.been.calledWith('one')
+                .to.have.been.calledWith('two');
+        });
+    });
+});
